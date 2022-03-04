836034034366c3d39e977bcab9394d8c7cea2d67
Martin Massé <martin@nuages.org>
2022-03-04 09:12:00 -0500

    ok

---
+++ Nuages.AWS/Secrets/AWSSecretProvider.cs
@@ -0,0 +1,37 @@
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+namespace Nuages.AWS.Secrets;
+
+// ReSharper disable once InconsistentNaming
+public class AWSSecretProvider : IAWSSecretProvider
+{
+    private readonly IAmazonSecretsManager _secretsManager;
+
+    public AWSSecretProvider(IAmazonSecretsManager secretsManager)
+    {
+        _secretsManager = secretsManager;
+    }
+    
+    public async Task<string> GetValueAsync(string secretArn)
+    {
+        if (!secretArn.StartsWith("arn:aws:secretsmanager"))
+            return secretArn;
+        
+        var request = new GetSecretValueRequest
+        {
+            SecretId = secretArn
+        };
+
+        var response = _secretsManager.GetSecretValueAsync(request).Result;
+
+        if (response.SecretString != null)
+        {
+            return response.SecretString;
+        }
+        
+        await using var memoryStream = response.SecretBinary;
+        var reader = new StreamReader(memoryStream);
+        return System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(await reader.ReadToEndAsync()));
+    }
+}
--- Nuages.AWS/Secrets/IAWSSecretProvider.cs
+++ Nuages.AWS/Secrets/IAWSSecretProvider.cs
@@ -1,6 +1,3 @@
-using Amazon.SecretsManager;
-using Amazon.SecretsManager.Model;
-
 namespace Nuages.AWS.Secrets;
 
 // ReSharper disable once InconsistentNaming
@@ -10,34 +7,3 @@ public interface IAWSSecretProvider
 }
 
 // ReSharper disable once InconsistentNaming
-public class AWSSecretProvider : IAWSSecretProvider
-{
-    private readonly IAmazonSecretsManager _secretsManager;
-
-    public AWSSecretProvider(IAmazonSecretsManager secretsManager)
-    {
-        _secretsManager = secretsManager;
-    }
-    
-    public async Task<string> GetValueAsync(string secretArn)
-    {
-        if (!secretArn.StartsWith("arn:aws:secretsmanager"))
-            return secretArn;
-        
-        var request = new GetSecretValueRequest
-        {
-            SecretId = secretArn
-        };
-
-        var response = _secretsManager.GetSecretValueAsync(request).Result;
-
-        if (response.SecretString != null)
-        {
-            return response.SecretString;
-        }
-        
-        await using var memoryStream = response.SecretBinary;
-        var reader = new StreamReader(memoryStream);
-        return System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(await reader.ReadToEndAsync()));
-    }
-}
